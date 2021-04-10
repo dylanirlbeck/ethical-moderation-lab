2ff2dbb3189b71c099f510a73e5635332caf74dc
Sahan Kumarasinghe <sahansk2@illinois.edu>
2021-04-10 15:39:10 -0400

    Added some comments about user-filtered code

--- notebook-prototype.py
+++ notebook-prototype.py
@@ -14,13 +14,17 @@ from lab import data_tools, display
 
 # %% [markdown]
 # Code block that parses data into the program
+# Put in the labels your group thinks is appropriate for the titles
 
 # %%
 training_data = data_tools.parse_data('data.txt')
 
+
+# %%
+# Between 10 and 20 of edge case examples to prompt the student
 training_data.extend([
-    ('y', 'A passable title'),
-    ('n', 'With the upcoming Thursday night NFL game, remember that this presents a simplified view of an entire culture, caricatures facial features based on race, depicts an outdated/inaccurate style of headdress, paints them as warmongering aggressors and overly glamorizes the violent side of their history.')
+    ('y/n', 'A passable title'),
+    ('y/n', 'With the upcoming Thursday night NFL game, remember that this presents a simplified view of an entire culture, caricatures facial features based on race, depicts an outdated/inaccurate style of headdress, paints them as warmongering aggressors and overly glamorizes the violent side of their history.')
 ])
 
 display.display_training_data(training_data)
